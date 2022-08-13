c621165e3c3d09a0735c4691f2bbc7291186a6b0
BrevalF <breval.ferrari@gmail.com>
2022-08-14 01:00:34 +0200

    goulab

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from time import sleep
-from tkinter import Button,Label,PhotoImage,Tk
+# move at four edges of screen before going back to center
+from tkinter import Label,Tk
 from PIL import Image,ImageTk
 def SetBG():
    global g
@@ -20,11 +20,11 @@ def Glimpse(geometry:str,own:bool=False):
    SetBG()
    bg.place(x=-5,y=-5)
    return tk
-def move(w:Tk,x:int=0,y:int=0):
+def move(w:Tk,dx,dy,speed,x:int=0,y:int=0):
    w.geometry(f'+{x}+{y}')
-   w.after(1,lambda:move(w,x+1,y))
+   w.after(speed,lambda:move(w,dx,dy,speed,x+dx,y+dy))
 d=Glimpse('100x100+50+50',True)
 d.bind('<Control-q>',lambda x:d.destroy())
 # d.bind('<Button-1>',lambda x:d.geometry(f'+{x.x*10}+{x.y*10}'))
-move(d)
+move(d,1,3,1)
 d.mainloop()
