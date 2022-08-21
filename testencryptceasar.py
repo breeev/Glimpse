70615b4231b5e0b28cac98eb17335d70d837944f
BrevalF <breval.ferrari@gmail.com>
2022-08-21 19:09:09 +0200

    rzrefz

--- testencryptceasar.py
+++ testencryptceasar.py
@@ -1,10 +1,9 @@
 from codecs import decode
-
-
 string=bytearray(b"""To be fair, said the cat, I do not intend to steal you.
 I would rather see you dead!
 - Alright.""")
-for n in range(len(string)):string[n]=(string[n]+n)%128
-# print(decode(string))
-for n in range(len(string)):string[n]=(string[n]-n)%128
+for n in range(len(string)):string[n]=(string[n]+n)%127
+with open('text','wb') as f:f.write(string)
+with open('text','rb') as f:string=bytearray(f.read())
+for n in range(len(string)):string[n]=(string[n]-n)%127
 print(decode(string))
